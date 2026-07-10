37dff1079e0a7a689ce942dc209a0a618438150d
cbkii <150587541+cbkii@users.noreply.github.com>
2026-07-11 09:49:01 +1000

    Complete TS18 nativeness hardening: app/src/test/java/com/openlauncher/app/headunit/HeadUnitProfileDetectorTest.kt

--- app/src/test/java/com/openlauncher/app/headunit/HeadUnitProfileDetectorTest.kt
+++ app/src/test/java/com/openlauncher/app/headunit/HeadUnitProfileDetectorTest.kt
@@ -5,9 +5,43 @@ import org.junit.Assert.assertEquals
 
 class HeadUnitProfileDetectorTest {
     @Test
-    fun testEnumValuesExist() {
-        assertEquals("TopwayTs18Dofun", HeadUnitProfile.TopwayTs18Dofun.name)
-        assertEquals("StandardAndroid", HeadUnitProfile.StandardAndroid.name)
-        assertEquals("Szchoiceway", HeadUnitProfile.Szchoiceway.name)
+    fun noEvidence_isStandardAndroidWithNoConfidence() {
+        assertEquals(
+            HeadUnitProfile.StandardAndroid to DetectionConfidence.NONE,
+            classifyHeadUnitProfile(emptySet())
+        )
+    }
+
+    @Test
+    fun genericTwEvidence_isUnknownRatherThanAFalsePositive() {
+        assertEquals(
+            HeadUnitProfile.UnknownHeadUnit to DetectionConfidence.LOW,
+            classifyHeadUnitProfile(setOf("com.tw.bt", "com.tw.eq"))
+        )
+    }
+
+    @Test
+    fun dofunAndCoreEvidence_isHighConfidenceTs18() {
+        val present = setOf(
+            "com.dofun.variety",
+            "com.tw.service",
+            "com.tw.core",
+            "com.tw.coreservice",
+            "com.tw.bt",
+            "com.tw.eq"
+        )
+
+        assertEquals(
+            HeadUnitProfile.TopwayTs18Dofun to DetectionConfidence.HIGH,
+            classifyHeadUnitProfile(present)
+        )
+    }
+
+    @Test
+    fun szchoicewayEvidence_isKeptSeparateFromTs18() {
+        assertEquals(
+            HeadUnitProfile.Szchoiceway to DetectionConfidence.HIGH,
+            classifyHeadUnitProfile(setOf("com.szchoiceway.radio", "com.szchoiceway.eventcenter"))
+        )
     }
 }
